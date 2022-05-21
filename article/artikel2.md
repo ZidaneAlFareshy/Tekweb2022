1105d2596f46497a4bab2469c97471dbdb1e1c8b
M. Jindan Dubbay A F <97089627+ZidaneAlFareshy@users.noreply.github.com>
2022-05-21 07:48:37 +0700

    Add files via upload

---
+++ article/artikel2.md
@@ -0,0 +1,7 @@
+Apa Itu User Experience?
+
+Singkatnya, user experience adalah bagaimana pengalaman pengguna dalam berinteraksi/menggunakan produk digital Anda. Pengalaman ini dilihat dari betapa mudahnya pengguna untuk mendapatkan apa yang mereka inginkan dari produk tersebut.
+
+Dengan kata lain, user experience produk yang bagus tidak akan menyulitkan pengguna untuk mencapai tujuan mereka. Entah itu dari desain UI yang friendly, produk yang ringan untuk diakses, menu yang tidak berbelit-belit, dan lain sebagainya. 
+
+Sebaliknya, user experience yang buruk membuat pengguna frustasi karena kesulitan untuk mendapatkan apa yang mereka inginkan. Kalau sudah begini, ada banyak efek buruk yang bisa terjadi kepada Anda.
---
+++ js/get_json.js
@@ -0,0 +1,44 @@
+Vue.createApp({
+    data() {
+      return {
+        artikel: [],
+        judul:"Artikel",
+        article:"a"
+      };
+    },
+    methods: {
+      getArtikel() {
+        axios
+          .get(
+            src="https://raw.githubusercontent.com/syawal07/Tekweb2022/master/js/article.json")
+          .then((res) => {
+            console.log(res.data);
+            this.artikel = res.data; 
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+      ambiljudul(){
+        const queryString = window.location.search;
+          const urlParams = new URLSearchParams(queryString);
+          const article = urlParams.get('article');        
+          var converter = new showdown.Converter();
+          axios
+            .get(
+              src="../article/"+article
+            )
+            .then((res) => {           
+              var html = converter.makeHtml(res.data);           
+              this.article = html;
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+      }
+    },
+    beforeMount() {
+      this.getArtikel();
+      this.ambiljudul();
+    },
+  }).mount("#app");
